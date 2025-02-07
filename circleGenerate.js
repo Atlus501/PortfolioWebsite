b1de41b81eb45757b24df9ba474ff30697f62df2
Atlus501 <helloworld4846@gmail.com>
2025-02-07 01:57:21 -0700

    commit push

--- circleGenerate.js
+++ circleGenerate.js
@@ -5,8 +5,8 @@ function createCircle() {
 
     bubble.className = "bubble";
 
-    const x = Math.random() * (100);
-    const y = Math.random() * (100);
+    const x = Math.random() * (70)+15;
+    const y = Math.random() * (80)+10;
 
     let duration = Math.random() * 6 + 2;
     let delay = Math.random() * 2.5 + 2.5;
@@ -14,12 +14,14 @@ function createCircle() {
     bubble.style.left = `${x}%`;
     bubble.style.top = `${y}%`;
 
+    bubble.style.backgroundColor = 'rgba('+255*x*.01+', 0, '+255*y*.01+', 0.6)';
+
     bubble.style.animation = 'pop '+duration+'s '+delay+'s ease-out infinite';
 
     background.appendChild(bubble);
 }
 
 // Create multiple circles
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 13; i++) {
     createCircle();
 }
